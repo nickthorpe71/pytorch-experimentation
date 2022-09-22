fc2662e27ae13d7ce633e19ebba3908d02bd0a4e
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-22 09:00:01 -0500

    add more tensor aggregation

--- tensor_aggregation.py
+++ tensor_aggregation.py
@@ -8,3 +8,18 @@ print(torch.min(x))
 # or
 print(x.min())
 
+# Find max
+print(torch.max(x))
+# or
+print(x.max())
+
+# Find mean
+print(torch.mean(x.type(torch.float32)))
+# or
+print(x.type(torch.float32).mean())
+
+# Find sum
+print(torch.sum(x))
+# or
+print(x.sum())
+
