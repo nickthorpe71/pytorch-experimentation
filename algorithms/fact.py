2ffcd30c4b263f126d05d61f81999c51ca287334
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-24 09:26:47 -0500

    add example fact algo

---
+++ algorithms/fact.py
@@ -0,0 +1,17 @@
+def iter_fact(n):
+    fact = 1
+    for i in range(2, n + 1):
+        fact *= i
+    return fact
+
+print(iter_fact(5))
+
+def rec_fact(n):
+    if n == 1:
+        return n;
+    else:
+        temp = rec_fact(n-1)
+        temp = temp * n
+    return temp
+
+print(rec_fact(5))
