e9722dd59e598cb924188a25c2171d2470229edd
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-26 09:30:29 -0500

    add torch to numpy example

--- numpy_test.py
+++ numpy_test.py
@@ -3,9 +3,12 @@ import numpy as np
 
 # Change np data into torch tensor
 array = np.arange(1.0, 8.0)
-tensor = torch.from_numpy(array)
+tensor = torch.from_numpy(array) # creates a copy of the original array
 print(array)
 print(tensor)
 
 # Change torch tensor into np data 
-
+tensor = torch.ones(7)
+numpy_tensor = tensor.numpy()
+print(tensor)
+print(numpy_tensor)
