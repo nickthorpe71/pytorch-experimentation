ac50be17324263a344941d7b4d57f813416e4c44
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-27 09:05:07 -0500

    add workflow files

---
+++ pytorch_workflow/intro.py
@@ -0,0 +1,16 @@
+import torch
+# from torch import nn # nn contains all of PyTorch's building blocks for neural networks
+# import matplotlib.pyplot as plt
+
+print(torch.__version__)
+
+what_were_covering = {
+    1: "data (prepare and load",
+    2: "build model",
+    3: "fitting the model to data (training)",
+    4: "making predictions and evaluating a model (inference)",
+    5: "saving and loading a model",
+    6: "putting it all together"
+}
+
+print(what_were_covering)
