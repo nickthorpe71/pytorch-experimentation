6e837a00b6a4b76dd59860470fc4dbbf152ed7ed
Nick Thorpe <nickthorpe71@gmail.com>
2022-09-29 09:00:04 -0500

    add plot example

--- pytorch_workflow/preparing_and_loading_data.py
+++ pytorch_workflow/preparing_and_loading_data.py
@@ -1,7 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
 
-
 # Create known parameters
 weight = 0.7
 bias = 0.3
@@ -24,4 +23,26 @@ train_split = int(0.8 * len(X))
 X_train, y_train = X[:train_split], y[:train_split]
 X_test, y_test = X[train_split:], y[train_split:]
 
-plt()
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train,
+                     test_data=X_test,
+                     test_labels=y_test,
+                     predictions=None):
+  """
+  Plots training data, test data and compares predictions.
+  """
+  plt.figure(figsize=(10,7))
+  
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+  
+  # Plot test data in green
+  plt.scatter(test_data, test_labels, c="g", s=4, label="Test data")
+  
+  if predictions is not None:
+    plt.scatter(test_data, predictions, c="r", s=4, labe="Predictions")
+    
+  plt.legend(prop={"size": 14})
+  plt.show(block=True)
+
+plot_predictions()
