bff702a78dc31c933a110964c63adbe4994420dd
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-01 13:01:00 -0500

    add training and test loop examples

--- pytorch_workflow/preparing_and_loading_data.py
+++ pytorch_workflow/preparing_and_loading_data.py
@@ -13,16 +13,21 @@ step = 0.02
 X = torch.arange(start, end, step).unsqueeze(dim=1)  # add an extra dimension
 y = weight * X + bias
 
-print(X[:10])
-print(y[:10])
-print(len(X))
-print(len(y))
+# print(X[:10])
+# print(y[:10])
+# print(len(X))
+# print(len(y))
 
 
 # Splitting data into training and test sets
 train_split = int(0.8 * len(X))
 X_train, y_train = X[:train_split], y[:train_split]
 X_test, y_test = X[train_split:], y[train_split:]
+
+# print(len(X_train)) 
+# print(len(y_train)) 
+# print(len(X_test)) 
+# print(len(y_test))
 
 def plot_predictions(train_data=X_train, 
                      train_labels=y_train,
@@ -41,13 +46,13 @@ def plot_predictions(train_data=X_train,
   plt.scatter(test_data, test_labels, c="g", s=4, label="Test data")
   
   if predictions is not None:
-    plt.scatter(test_data, predictions, c="r", s=4, labe="Predictions")
+    plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
     
   plt.legend(prop={"size": 14})
   plt.savefig("output.jpg")
   
 # How to get plot to render in wsl2: https://stackoverflow.com/questions/43397162/show-matplotlib-plots-and-other-gui-in-ubuntu-wsl1-wsl2
-plot_predictions()
+# plot_predictions()
 
 class LinearRegressionModel(nn.Module):
   def __init__(self):
@@ -61,3 +66,75 @@ class LinearRegressionModel(nn.Module):
   
   def forward(self, x: torch.Tensor) -> torch.Tensor:
     return self.weights * x + self.bias # this is the linear regression formula
+  
+  
+# Create a random seed to keep example consistant every run
+torch.manual_seed(11)
+
+# Create an instance of the modael 
+model_0 = LinearRegressionModel()
+
+# print(list(model_0.parameters()))
+# print(model_0.state_dict())
+
+# Make predictions with model
+
+with torch.inference_mode():
+  y_preds = model_0(X_test)
+  
+# print(y_preds)
+
+# plot_predictions(predictions=y_preds)
+
+# Set up a loss funciton
+loss_fn = nn.L1Loss()
+
+# Set up an optimizer
+optimizer = torch.optim.SGD(params=model_0.parameters(),
+                            lr=0.01) # lr = learning rate
+
+
+# Build a training loop
+
+# An epoch is one loop through the data...
+epochs = 500
+
+# Loop through the data
+for epoch in range(epochs):
+  # Set the model to training mode
+  model_0.train() # train mode turns on gradient descent
+  
+  # Forward pass
+  y_pred = model_0(X_train)
+  
+  # Calculate the loss
+  loss = loss_fn(y_pred, y_train)
+  
+  # Optimizer zero grad
+  optimizer.zero_grad()
+  
+  # Back propagation
+  loss.backward()
+  
+  # Gradient descent
+  optimizer.step()
+  
+  
+  # Testing
+  model_0.eval() # eval mode turns off gradient descent
+  with torch.inference_mode():
+    # Do forward pass
+    test_pred = model_0(X_test)
+    
+    # Calculate the loss
+    test_loss = loss_fn(test_pred, y_test)
+  
+  if epoch % 10 == 0:
+    print(f"Epoch: {epoch} | Loss: {loss} | Test loss: {test_loss}")  
+    print(model_0.state_dict())
+  
+  
+with torch.inference_mode():
+  y_preds_new = model_0(X_test)
+  
+plot_predictions(predictions=y_preds_new)
