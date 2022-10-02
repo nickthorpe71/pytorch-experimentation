39d2361bfa711c7591eea7757d09c181e1389a51
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-02 08:40:16 -0500

    add loss curve example

--- pytorch_workflow/preparing_and_loading_data.py
+++ pytorch_workflow/preparing_and_loading_data.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torch import nn
 import matplotlib.pyplot as plt
 
@@ -69,7 +70,7 @@ class LinearRegressionModel(nn.Module):
   
   
 # Create a random seed to keep example consistant every run
-torch.manual_seed(11)
+torch.manual_seed(42)
 
 # Create an instance of the modael 
 model_0 = LinearRegressionModel()
@@ -97,7 +98,12 @@ optimizer = torch.optim.SGD(params=model_0.parameters(),
 # Build a training loop
 
 # An epoch is one loop through the data...
-epochs = 500
+epochs = 200
+
+# Track different values
+epoch_count = []
+loss_values = []
+test_loss_values = []
 
 # Loop through the data
 for epoch in range(epochs):
@@ -130,10 +136,23 @@ for epoch in range(epochs):
     test_loss = loss_fn(test_pred, y_test)
   
   if epoch % 10 == 0:
+    epoch_count.append(epoch)
+    loss_values.append(loss)
+    test_loss_values.append(test_loss)
     print(f"Epoch: {epoch} | Loss: {loss} | Test loss: {test_loss}")  
     print(model_0.state_dict())
-  
-  
+    
+
+# Plot loss curves
+plt.plot(epoch_count, np.array(torch.tensor(loss_values).numpy()), label="Train loss")
+plt.plot(epoch_count, test_loss_values, label="Test loss") 
+plt.title("Training and test loss curves")
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.legend()
+
+plt.savefig("loss_curve.jpg")
+ 
 with torch.inference_mode():
   y_preds_new = model_0(X_test)
   
