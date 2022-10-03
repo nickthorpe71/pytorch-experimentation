dd94ed26c3a57dacd152b9572d5e2696632169e9
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-03 08:06:37 -0500

    add data split for classification

--- classification/data_setup.py
+++ classification/data_setup.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import make_circles
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
 
 n_samples = 1000
 
@@ -26,3 +28,24 @@ plt.scatter(x=X[:, 0],
 
 plt.savefig("output.jpg")
 
+# Check input and output shapes
+print(X.shape, y.shape)
+
+# View the first example of features and labels
+X_sample = X[0]
+y_sample = y[0]
+
+# We heave 2 features of X trying to predict 1 value y
+print(X_sample)
+print(y_sample)
+
+# Turn data into tensors
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float) 
+
+print(X[:5], y[:5])
+
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(len(X_train), len(X_test), len(y_train), len(y_test))
