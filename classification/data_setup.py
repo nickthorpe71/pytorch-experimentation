2e06b0de9bcbc940c5cf7616c56e9215092ded20
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-08 17:19:03 -0500

    add example classification mdoels:

--- classification/data_setup.py
+++ classification/data_setup.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
+from torch import nn
 
 n_samples = 1000
 
@@ -49,3 +50,34 @@ print(X[:5], y[:5])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 print(len(X_train), len(X_test), len(y_train), len(y_test))
+
+
+
+# device agnostic
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# build a model
+class CircleModelV0(nn.Module):
+  def __init__(self):
+    super().__init__()
+    # create 2 nn.Linear layers capable of handling the shapes of our data
+    self.layer_1 = nn.Linear(in_features=2, out_features=5)
+    self.layer_2 = nn.Linear(in_features=5, out_features=1)
+  
+  # define a forward() method that outlines the forward pass
+  def forward(self, x):
+    return self.layer_2(self.layer_1(x))
+  
+# instantiate an instance of our model class and send to target device
+model_0 = CircleModelV0().to(device)
+print(model_0)
+print(next(model_0.parameters()).device)
+
+
+# create the same model in less code
+model_1 = nn.Sequential(
+  nn.Linear(in_features=2, out_features=5),
+  nn.Linear(in_features=5, out_features=1)
+).to(device)
+
+print(model_1)
