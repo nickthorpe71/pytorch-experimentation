3d29c05f171b21270ec253b531789feddcf05133
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-09 14:16:59 -0500

    add more fundamentals practice

--- fundamentals/c2_torch_fundamentals.py
+++ fundamentals/c2_torch_fundamentals.py
@@ -11,3 +11,23 @@ x = torch.rand(2,2)
 y = torch.rand(2,2)
 print(x)
 print(y)
+
+z = x + y
+z = torch.add(x,y)
+print(z)
+
+# print all rows and first column
+print(x[:, 0])
+
+# print all columns and second row
+print(x[1, :])
+
+# print element at pos 1,1
+print(x[1,1])
+
+# resize tensors
+x = torch.rand(4,4)
+print(x)
+y = x.view(16, 1)
+print(y)
+y = x.view(2, 8)
