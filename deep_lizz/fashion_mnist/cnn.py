be35d4503667e182d0f19f2f2126e2e6fc26f16b
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-30 19:50:51 -0500

    add cnn example class

---
+++ deep_lizz/fashion_mnist/cnn.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch.functional as F
+
+
+class Network(nn.Module):
+    r"""
+    Convolutional neural network (CNN) for Fashion-MNIST dataset.
+    """
+
+    def __init__(self):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+
+        self.fc1 = nn.Linear(in_features=12*4*4, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=60)
+        self.out = nn.Linear(in_features=60, out_features=10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+network = Network()
+print(network)
