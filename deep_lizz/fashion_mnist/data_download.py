ed1c9f18c9688ccb42e4eccfe7f9a1d9c36f3548
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-30 19:27:20 -0500

    add data loading example

--- deep_lizz/fashion_mnist/data_download.py
+++ deep_lizz/fashion_mnist/data_download.py
@@ -1,6 +1,10 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+torch.set_printoptions(linewidth=120)
 
 train_set = torchvision.datasets.FashionMNIST(
     root='./data/FashionMNIST',
@@ -11,4 +15,22 @@ train_set = torchvision.datasets.FashionMNIST(
     ])
 )
 
-train_loader = torch.utils.data.DataLoader(train_set)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=100)
+
+sample = next(iter(train_set))
+
+image, label = sample
+
+# plt.imshow(image.squeeze(), cmap='gray')
+# plt.savefig('sample.png')
+# print('label:', label)
+
+batch = next(iter(train_loader))
+images, labels = batch
+
+grid = torchvision.utils.make_grid(images, nrow=10)
+
+plt.figure(figsize=(15, 15))
+plt.imshow(np.transpose(grid, (1, 2, 0)))
+plt.savefig('sample.png')
+print('labels:', labels)
