c5bdaeb9fecc65207931c6366b892f94bd5a02d9
Nick Thorpe <nickthorpe71@gmail.com>
2022-11-05 08:37:53 -0500

    add upsampling example and GANs folder

---
+++ deep_lizz/GANs/upsampling.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+# import torch.nn.functional as F
+
+t = torch.arange(1,5).float()
+print(t)
+t = t.reshape(1,1,2,2)
+print(t)
+
+upsample_nearest = nn.Upsample(scale_factor=2, mode='nearest')
+t_nearest = upsample_nearest(t)
+print(t_nearest)
+
+upsample_bilinear = nn.Upsample(scale_factor=2, mode='bilinear')
+t_bilinear = upsample_bilinear(t)
+print(t_bilinear)
