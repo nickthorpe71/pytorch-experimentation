8f46a25e034f830bd13bec92e74e904811050942
Nick Thorpe <nickthorpe71@gmail.com>
2022-11-13 10:39:59 -0600

    add differend exmaples of random distribution for uniform and standard normal

--- deep_lizz/GANs/torch_random_noise.py
+++ deep_lizz/GANs/torch_random_noise.py
@@ -5,10 +5,11 @@ import pandas as pd
 torch.manual_seed(0)
 torch.set_printoptions(linewidth=120)
 
-# Create a tensor of 100 random numbers
-noise_vector_1 = torch.randn(100)
+# Create a tensor of 100 random numbers from a random uniform distribution over the interval [0, 1)]
+noise_vector_1 = torch.rand(100)
 print(noise_vector_1)
 
+# Create a tensor of 100 random numbers from a random standard normal distribution with mean 0 and standard deviation 1
 noise_vector_2 = torch.randn(100)
 print(noise_vector_2)
 
